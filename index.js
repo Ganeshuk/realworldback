bd37466546614cb742c608b96c78021e52fa3d24
Ganeshuk <113826479+Ganeshuk@users.noreply.github.com>
2024-09-14 11:11:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.put('/movie/:id', async (req, res) => {
       
   });
 app.get('/',async(req,res)=>{
-    const qurey=`SELECT DISTINCT movieName, moviePoster FROM list ORDER BY id DESC`
+    const qurey=`SELECT DISTINCT movieName, moviePoster,id FROM list ORDER BY id DESC`
     const result=await client.query(qurey)
     res.json(result.rows)
 })
